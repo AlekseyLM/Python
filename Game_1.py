e79e87b7bb082f721db2e38a32e3b792a02bc79b
AlekseyLM <almaz.66@gmail.com>
2013-12-01 21:02:51 +0600

    Замена некоторых переменных

--- Game_1.py
+++ Game_1.py
@@ -1,11 +1,11 @@
 #   Определяем начальные значения переменных
 min_max = [0, 1000]
-otvet = (min_max[0] + min_max[1]) / 2
+vvod = (min_max[0] + min_max[1]) / 2
 v_text = {1:'нижний предел', 2:'верхний предел', 3:'отгадываемое число'}
 
 #   Создаем функцию проверки ввода
 def prov_vvoda(vd_text):
-    global otvet
+    global vvod
     while True:
         sp = input('Введите ' + v_text[vd_text] + ' (от ' + str(min_max[0]) + ', до ' + str(min_max[1]) + ')\nили нажмите "Enter" для выхода:')
         if sp == '':
@@ -13,17 +13,17 @@ def prov_vvoda(vd_text):
         elif not sp.isdigit():
             print('Ну это же не положительное число!')
         else:
-            otvet = int(sp)
-            if min_max[0] <= otvet <= min_max[1]:
+            vvod = int(sp)
+            if min_max[0] <= vvod <= min_max[1]:
                 break
             else:
                 print('Число не соответствует условию (от ', min_max[0], ', до ', min_max[1], ')', sep = '')
 
 #   Вводим пределы min_max, проверяем ввод
 prov_vvoda(1)
-min_max[0] = otvet
+min_max[0] = vvod
 prov_vvoda(2)
-min_max[1] = otvet
+min_max[1] = vvod
 
 #   Генерируем случайное число в диапазоне min_max
 import random
@@ -35,13 +35,13 @@ i_step = 1
 while True:
     print('Попытка №', i_step, sep='')
     prov_vvoda(3)
-    if r_vopros == otvet:
-        print('Отлично! Загаданное число "', otvet, '" Вы угадали с ', i_step, '-й попытки!', sep = '')
+    if r_vopros == vvod:
+        print('Отлично! Загаданное число "', vvod, '" Вы угадали с ', i_step, '-й попытки!', sep = '')
         input('Нажмите "Enter" для выхода.')    
         raise SystemExit(1)
-    elif r_vopros > otvet:
-        if otvet > min_max[0]: min_max[0] = otvet
+    elif r_vopros > vvod:
+        if vvod > min_max[0]: min_max[0] = vvod
     else:
-        if otvet < min_max[1]: min_max[1] = otvet
+        if vvod < min_max[1]: min_max[1] = vvod
     i_step += 1
 input('Нажмите "Enter" для выхода.')
